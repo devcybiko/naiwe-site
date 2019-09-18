12855bb492c3373c029b0c6614e93f1b4aa40690
Greg Smith <greg@thinkable.us>
2019-09-17 23:03:03 -0400

    auto-updating

--- gatherlogs.sh
+++ gatherlogs.sh
@@ -3,7 +3,7 @@ find ~/public_html -name 'error_log' -ls -exec ~/git/naiwe-logs/cp.sh {} ';'
 cd ~/git/naiwe-logs
 date > lastupdate.txt
 id > id.txt
-git pull origin master
+git pull origin/master
 git add -A
 git commit -am "auto-updating"
 git push
