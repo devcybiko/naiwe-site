a4adc3929e0b99dd9c30e455afbf3d9cccd96ca1
Greg Smith <greg@thinkable.us>
2019-09-17 23:24:31 -0400

    auto-updating Tue Sep 17 23:24:31 EDT 2019

--- gatherlogs.sh
+++ gatherlogs.sh
@@ -2,7 +2,7 @@
 find ~/public_html -name 'error_log' -ls -exec ~/git/naiwe-logs/cp.sh {} ';'
 cd ~/git/naiwe-logs
 theDate=`date`
-echo "$theDate" > lastUpdate.txt
+echo "$theDate" > lastupdate.txt
 id > id.txt
 #git pull
 git add -A
